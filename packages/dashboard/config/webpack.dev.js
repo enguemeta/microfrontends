090a32b78b88f712dd1decdfb6a63e47fd4f5d33
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2021-02-28 12:41:53 -0500

    Adding dashboard app

---
+++ packages/dashboard/config/webpack.dev.js
@@ -0,0 +1,37 @@
+const { merge } = require("webpack-merge");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  output: {    
+    publicPath: "http://localhost:8083/"
+  },
+  devServer: {
+      port: 8083,
+      historyApiFallback: {
+          index: "index.html"
+      },
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+  }, 
+  plugins: [
+      new ModuleFederationPlugin({
+          name: "dashboard",
+          filename: "remoteEntry.js",
+          exposes: {
+            "./DashboardApp" : "./src/bootstrap"
+        },
+        shared : packageJson.dependencies,
+      }),
+      new HtmlWebPackPlugin({
+          template: "./public/index.html"
+      })
+  ]
+};
+
+module.exports = merge(commonConfig, devConfig);
